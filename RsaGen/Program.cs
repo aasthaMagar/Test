6e8e70330461b3898740da4cd636886d64c046cc
aastha.magar <aastha.magar@nepalpayment.com>
2024-06-23 12:46:15 +0545

    test

---
+++ RsaGen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.Serialization;
+
+
+namespace ConsoleApp
+{
+    public class RsaEncryption
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RsaEncryption()
+        {
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+        }
+
+        public string GetPublicKey()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, _publicKey);
+            return sw.ToString();
+        }
+
+    public string Encrypt(string plainText)
+    {
+        csp = new RSACryptoServiceProvider();
+        csp.ImportParameters(_publicKey);
+        var data = Encoding.Unicode.GetBytes(plainText);
+        var cypher = csp.Encrypt(data, false);
+    }
+
+}
